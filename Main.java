8b1b3a83947175eee46194063acd90dcabfe5f6f
Emin <emirkara21@gmail.com>
2022-08-09 18:26:48 +0300

    Son Hali

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int kacSayi, sayi = 1, min = 1, max = 1, a;
+
+        System.out.print("Kac tane sayi gireceksiniz: ");
+        kacSayi = inp.nextInt();
+
+        for (a = 1; a <= kacSayi; a++){
+            System.out.print(a + ".sayiyi giriniz: ");
+            sayi = inp.nextInt();
+
+            if (a == 1)
+            {max = a; min = a;}
+            else if (sayi > max)
+            {max = sayi;}
+            else if (sayi < min)
+            {min = sayi;}
+            }
+
+        System.out.println();
+        System.out.println("En Büyük Sayı: " + max);
+        System.out.println("En Küçük Sayı: " + min);
+        }
+    }
